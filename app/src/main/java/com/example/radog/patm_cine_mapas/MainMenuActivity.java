6274ab8874c7d08ef92f70232b7fcb36b45db6ec
cari1928 <13030622@itcelaya.edu.mx>
2017-05-18 03:05:28 -0500

    Volviendo al proyecto original

--- app/src/main/java/com/example/radog/patm_cine_mapas/MainMenuActivity.java
+++ app/src/main/java/com/example/radog/patm_cine_mapas/MainMenuActivity.java
@@ -1,6 +1,5 @@
 package com.example.radog.patm_cine_mapas;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -9,8 +8,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-
-import com.example.radog.patm_cine_mapas.rol.RolActivity;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -45,8 +42,7 @@ public class MainMenuActivity extends AppCompatActivity {
 
     @OnClick(R.id.ibtnTickets)
     public void ibtnTickets() {
-        Intent intent = new Intent(this, RolActivity.class);
-        startActivity(intent);
+
     }
 
     @OnClick(R.id.ibtnLog)
--- app/src/main/java/com/example/radog/patm_cine_mapas/proveedor/BitacoraProveedor.java
+++
@@ -1,8 +0,0 @@
-package com.example.radog.patm_cine_mapas.proveedor;
-
-/**
- * Created by radog on 15/05/2017.
- */
-
-public class BitacoraProveedor {
-}
